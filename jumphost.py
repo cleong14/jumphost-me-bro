a41605ac7577f01e446b0e279072b380e9ab9ca0
Chaz <cleong14@gmail.com>
2018-12-04 19:36:47 -1000

    Got ssh user and password for host. Working on scripting...

--- jumphost.py
+++ jumphost.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
 
+import paramiko
+import sys
+import os
 import socket
 
-target_host = '104.248.191.147'
-target_port = 80
+global host, username, line, input_file
 
-# create a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+line = '\n' + '-'*50 + '\n'
 
-# send some data
-client.sendto("DATA",(target_host,target_port))
-
-# receive some data
-
-data, addr = client.recvfrom(4096)
-
-print('DATA', data)
